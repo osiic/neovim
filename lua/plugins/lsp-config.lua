626bf3ff39390bc0b2900bec2121886a71a17cba
Osi ic <osiic.offcl@gmail.com>
2024-07-22 14:31:39 +0700

    set

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,46 @@
+-- LSP
+-- Mason = tempat dowload package nya
+-- Moson LSP Config = auto dowload pakage nya
+-- Nvim LSP Config = ngatur pakage nya
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "tsserver", "tailwindcss", "eslint" },
+        automatic_installation = true,
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.tsserver.setup({})
+      lspconfig.tailwindcss.setup({})
+      lspconfig.eslint.setup({})
+      lspconfig.lua_ls.setup({
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+          },
+        },
+      })
+
+      -- Keymap
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+}
+
---
+++ lua/plugins/null-ls.lua
@@ -0,0 +1,15 @@
+return {
+	"nvimtools/none-ls.nvim",
+  event = "VeryLazy",
+	config = function()
+		local null_ls = require("null-ls")
+
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+			},
+		})
+		vim.keymap.set("n", "<leader>s", vim.lsp.buf.format, {})
+	end,
+}
